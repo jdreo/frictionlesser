ea3020384ccbc333c5e39c4890f9f6a233d26ad6
Johann Dreo <johann.dreo@pasteur.fr>
2023-02-11 07:02:39 +0100

    fix(snake): move cluster options in config, increase mem and qos

--- scripts/paris/run-maestro.sh
+++ scripts/paris/run-maestro.sh
@@ -3,5 +3,7 @@
 module load Python/3.8.1
 module load snakemake
 
-sbatch --job-name "PARIS" snakemake --cluster "sbatch --mem 8000 --cpus-per-task=1 --partition common,dedicated --qos fast" --configfile config.yaml --jobs 15000
+mkdir -p slurmout
+
+sbatch --job-name "PARIS" snakemake --configfile config.yaml --jobs 15000
 
