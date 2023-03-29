9df9451d8bc5f053eacf369287ad04c8f390d7bf
Johann Dreo <johann.dreo@pasteur.fr>
2023-03-29 12:31:24 +0200

    fix submit-run

--- scripts/paris/submit-run.sh
+++ scripts/paris/submit-run.sh
@@ -5,13 +5,13 @@ seed=${2}
 
 FRICTIONLESSER="../../build/app/frictionlesser"
 
-${FRICTIONLESSER}
-    --ranks=data/inter/ranks.tsv
-    --cache-transcriptome=cache/trans.cache.dat
-    --cache-size=cache/size_${size}.cache.dat
-    --ngenes=${size}
-    --seed=${seed}
-    --save-sol=data/inter/trajs/frictionlesser_solutions_z${size}_s${seed}.tsv
-    > data/output/signature_of_${size}-genes/signature_${seed}
+${FRICTIONLESSER} \
+    --ranks=data/inter/ranks.tsv \
+    --cache-transcriptome=cache/trans.cache.dat \
+    --cache-size=cache/size_${size}.cache.dat \
+    --ngenes=${size} \
+    --seed=${seed} \
+    --save-sol=data/inter/trajs/frictionlesser_solutions_z${size}_s${seed}.tsv \
+    > data/output/signature_of_${size}-genes/signature_${seed} \
     2> data/inter/logs/frictionlesser_z${size}_s${seed}.log
 
