fe2b5c73206cdab4b5d4177a41978d7f496706bb
Johann Dreo <johann.dreo@pasteur.fr>
2023-04-28 11:14:43 +0200

    refactor correlation computation

--- scripts/paris/correlations_genes-samples.py
+++ scripts/paris/correlations_genes-samples.py
@@ -8,15 +8,23 @@ import signatures
 
 if __name__ == "__main__":
 
-    assert(len(sys.argv) >= 6)
+    assert(len(sys.argv) >= 8)
     franks = sys.argv[1]
     size = int(sys.argv[2])
     if size == 0:
         size = None
     fout_gcorr = sys.argv[3]
     fout_scorr = sys.argv[4]
-    fplot = sys.argv[5]
-    fsignatures = sys.argv[6:]
+    fplot_gcorr = sys.argv[5]
+    fplot_selfcorr = sys.argv[6]
+    fsignatures = sys.argv[7:]
+
+    # Precision for floating-point computation asserts.
+    epsilon = 1e-6
+
+    ###########################################################################
+    # LOAD
+    ###########################################################################
 
     print("Load annotated ranks data from: ", franks, file=sys.stderr, flush=True)
     adata = ad.read(franks)
@@ -38,13 +46,17 @@ if __name__ == "__main__":
     unique_signatures = OrderedSet()
     loaded_scores = {}
     unique_scores = {}
+    origins = {}
     for fsign in fsignatures:
         s,g,b,S = signatures.load([fsign], size, with_scores=True)
         loaded_signatures[fsign] = s
         genomes[fsign] = g
         breaks[fsign] = b
         loaded_scores[fsign] = S
-        unique_signatures |= s
+        for sign in s:
+            # origins.setdefault(" ".join(sorted(sign)),set()).add(fsign)
+            origins.setdefault(sign,set()).add(fsign)
+            unique_signatures |= s
         for sk in S:
             if __debug__:
                 if sk in unique_scores:
@@ -61,7 +73,11 @@ if __name__ == "__main__":
     nsamples = len(samples)
     assert(len(samples) > 0)
 
-    print("Compute genes correlations...", file=sys.stderr, flush=True)
+    ###########################################################################
+    # GENES CORRELATIONS
+    ###########################################################################
+
+    print("Compute cell-gene z-scores sample fractions...", file=sys.stderr, flush=True)
     corr_cellgene = ad.AnnData(
         np.zeros((ncells,len(genome))),
         adata.obs, {"gene":np.asarray(genome)},
@@ -71,31 +87,79 @@ if __name__ == "__main__":
     # print(ranks.shape, file=sys.stderr, flush=True)
     assert(ranks.shape == adata.X.shape)
     for i,s in enumerate(samples):
-        cells = adata.obs["sample"] == s
-        for j,g in enumerate(genome):
+        cells = (adata.obs["sample"] == s)
+        assert(np.sum(cells) > 1)
+        for j,g in enumerate(genome): # FIXME bitand merge truth tables for all genes to avoid this loop
             cranks = ranks[ cells, adata.var["id"] == g ]
-            nc = cranks.shape[0]
+            # print("cells ranks:",cranks, file=sys.stderr, flush=True)
+            assert(len(cranks.shape) == 1) # only one gene should match.
+            sn = cranks.shape[0]
+            assert(sn > 0)
+            # print("sn:",sn, file=sys.stderr, flush=True)
             smean = np.mean(cranks)
-            sdev  = np.std (cranks)
+            assert(smean == (sn+1)/2)
+            # sdev  = np.std (cranks) # sample mean
+            sdev = np.sqrt(np.sum(np.power(cranks-smean,2)))
             if sdev != 0:
-                corr_cellgene[cells,j] = np.clip(
-                        (cranks-smean)/sdev * 1/np.sqrt(nc-1) * 1/nsamples,
-                    -1, 1)
+                # Standardized z-score across cells,
+                # and pre-division for preparing the incoming dot product.
+                corr_cellgene[cells,j] = (cranks-smean)/sdev * 1/np.sqrt(nsamples)
+
+                # print("corr:",corr_cellgene[cells,j].X, file=sys.stderr, flush=True)
+
+                # FIXME why the sqrt?
+                # ssp2 = np.sqrt(np.sum(np.power(corr_cellgene[cells,j].X,2)))
+                ssp2 = np.sum(np.power(corr_cellgene[cells,j].X, 2)) * nsamples
+                # print("ssp2:", ssp2, file=sys.stderr, flush=True)
+                assert(np.abs(1-ssp2) <= epsilon)
             else:
                 corr_cellgene[cells,j] = np.zeros(cranks.shape)
+                assert(np.sum(np.power(corr_cellgene[cells,j].X, 2)) * nsamples == 0)
 
-    print("Save genes correlations...", file=sys.stderr, flush=True)
+    # Assert sum of scores for each gene.
+    for g in genome:
+        cgene = corr_cellgene[ :, corr_cellgene.var["gene"] == g ]
+        # print(np.sum(cgene.X, axis=1), file=sys.stderr, flush=True)
+        # assert(all([0 <= s <= 1 for s in np.sqrt(np.sum(np.power(cgene.X,2), axis=1))]))
+        assert(all(0 <= s <= 1 for s in np.sum(np.power(cgene.X,2), axis=1)))
+
+    print("Compute pairwise average correlation matrix for genes over samples...", file=sys.stderr, flush=True)
+    corr_cellgene.varp["correlations"] = corr_cellgene.X.T @ corr_cellgene.X
+    # print(np.diag(corr_cellgene.varp["correlations"]), file=sys.stderr, flush=True)
+
+    print("Save cell-gene z-score and gene correlations...", file=sys.stderr, flush=True)
     print(corr_cellgene, file=sys.stderr, flush=True)
     corr_cellgene.write(fout_gcorr, compression = "gzip")
 
-    print("Compute signatures correlations...", file=sys.stderr, flush=True)
+    ###########################################################################
+    # PLOT
+    ###########################################################################
+
+
+
+    ###########################################################################
+    # SIGNATURES CORRELATIONS
+    ###########################################################################
+
+    # for s in unique_signatures:
+    #     print("Uniq Sign:",s)
+    # for k in origins:
+    #     print("Origin:",k,"=>",origins[k])
+    var = {"signature":np.asarray([frozenset(s) for s in unique_signatures])}
+    aorig = []
+    for s in var["signature"]:
+        aorig.append(origins[s])
+    var["origin"] = np.asarray(aorig)
+
+    print("Compute cell-signatures average correlations over samples...", file=sys.stderr, flush=True)
     corr_cellsign = ad.AnnData(
         np.zeros((ncells,nsignatures)),
         adata.obs,
-        {"signature":np.asarray(unique_signatures)},
+        var,
         dtype=corr_cellgene.X.dtype )
 
     for signature in unique_signatures:
+        # FIXME annotate with origin file
         sgenes = np.zeros(ngenes, dtype=bool)
         for g in signature:
             # Logical "and" on locations of each genes.
@@ -111,22 +175,31 @@ if __name__ == "__main__":
         # Sum across genes.
         corr_cellsign[:, corr_cellsign.var["signature"] == signature] = csum
 
+    print("Compute pairwise correlation matrix for signatures...", file=sys.stderr, flush=True)
     # varp is a dictionary holding (var × var) arrays.
     # We want the dot product of all columns.
     corr_cellsign.varp["correlations"] = corr_cellsign.X.T @ corr_cellsign.X
 
     print(corr_cellsign, file=sys.stderr, flush=True)
-    print(corr_cellsign.varp["correlations"], file=sys.stderr, flush=True)
+    # print(corr_cellsign.varp["correlations"], file=sys.stderr, flush=True)
 
-    print("Save signatures correlations...", file=sys.stderr, flush=True)
+    print("Save cell-signatures correlations...", file=sys.stderr, flush=True)
     # AnnData cannot implicitely convert signature's OrderedSet to strings,
     # so we do it manually.
     ssigns = []
     for oset in corr_cellsign.var["signature"]:
         ssigns.append(" ".join(oset)) # Space-separated.
     corr_cellsign.var["signature"] = ssigns
+    origs = []
+    for orig in corr_cellsign.var["origin"]:
+        origs.append(" ".join(orig))
+    corr_cellsign.var["origin"] = origs
     print(corr_cellsign, file=sys.stderr, flush=True)
     corr_cellsign.write(fout_scorr, compression = "gzip")
+
+    ###########################################################################
+    # PLOT
+    ###########################################################################
 
     print("Plot self-correlations of signatures against scores...", file=sys.stderr, flush=True)
     selfcorr = np.diag(corr_cellsign.X)
@@ -144,6 +217,6 @@ if __name__ == "__main__":
     plot.set(xlabel="objective function", ylabel="self-correlation",
              title="Relationship between\nobjective function and self-correlation of genes, for signatures in\n{}".format(fsignatures))
     fig = plot.get_figure()
-    fig.savefig(fplot, dpi=600)
+    fig.savefig(fplot_selfcorr, dpi=600)
 
     print("Done", file=sys.stderr, flush=True)
--- scripts/paris/validation.Snakefile
+++ scripts/paris/validation.Snakefile
@@ -1,12 +1,7 @@
 
 rule all:
     input:
-        "data/qc/clustermap_correlations.png",
-        # "data/qc/clustermap_pvalues.png",
-        "data/qc/observed_genes.txt",
-        "data/qc/observed_genes_distribution.png",
-        "data/qc/selfcorr-score_local.png",
-        "data/qc/jaccard-distances.png"
+        "data/qc/self-corr_objf.png"
 
 rule aggregate:
     input:
@@ -16,98 +11,17 @@ rule aggregate:
     shell:
         "cat {input}/signature_* | sort | uniq > {output}"
 
-rule qc_observed_genes:
+rule correlations_genes_samples:
     input:
-        task="qc-observed-genes.py",
-        ranks="data/inter/ranks.tsv",
-        A="data/output/emil_all.csv",
-        B="data/output/signatures_z10.tsv"
+        task="correlations_genes-samples.py",
+        ranks="data/inter/paris+ranks.h5an.gz",
+        sign="data/output/signatures_z10.tsv"
     output:
-        text="data/qc/observed_genes.txt",
-        plot="data/qc/observed_genes_distribution.png"
+        gcorr="data/inter/gcorr.h5an.gz",
+        scorr="data/inter/scorr.h5an.gz",
+        # plot_gcorr="data/qc/genes_corr.png",
+        plot_selfcorr="data/qc/self-corr_objf.png",
     shell:
-        "python3 {input.task} 10 {input.ranks} {output.plot} {input.A} {input.B} > {output.text}"
+        # "python3 {input.task} {input.ranks} 10 {output.gcorr} {output.scorr} {output.plot_gcorr} {output.plot_selfcorr} {input.sign}"
+        "python3 {input.task} {input.ranks} 10 {output.gcorr} {output.scorr} tmp.png {output.plot_selfcorr} {input.sign}"
 
-rule genes_standardized_scores:
-# Two signatures sets.
-    input:
-        task="genes-standardized-over-samples__sign-to-npy.py",
-        ranks="data/inter/ranks.tsv",
-        A="data/output/emil_all.csv",
-        B="data/output/signatures_z10.tsv"
-    output:
-        scores="data/inter/genes-standardized-scores.npy",
-        indices="data/inter/genes-standardized-scores_genes.csv"
-    shell:
-        "python3 {input.task} {input.ranks} 10 {output.scores} {output.indices} {input.A} {input.B}"
-
-rule genes_standardized_scores_local:
-# Single signature set.
-    input:
-        task="genes-standardized-over-samples__sign-to-npy.py",
-        ranks="data/inter/ranks.tsv",
-        signs="data/output/signatures_z10.tsv"
-    output:
-        scores="data/inter/genes-standardized-scores_local.npy",
-        indices="data/inter/genes-standardized-scores_genes_local.csv"
-    shell:
-        "python3 {input.task} {input.ranks} 10 {output.scores} {output.indices} {input.signs}"
-
-rule qc_selfcorr_score_local:
-    input:
-        task="qc_selfcorr-score.py",
-        ranks="data/inter/ranks.tsv",
-        scores="data/inter/genes-standardized-scores_local.npy",
-        indices="data/inter/genes-standardized-scores_genes_local.csv",
-        signs="data/output/signatures_z10.tsv"
-    output:
-        corr="data/inter/signatures-average-selfcorrelations_local.npy",
-        plot="data/qc/selfcorr-score_local.png"
-    shell:
-        "python3 {input.task} {input.ranks} {input.scores} {input.indices} 10 {output.corr} {output.plot} {input.signs}"
-
-rule signatures_correlations:
-    input:
-        task="signatures-correlations__sign-to-npy.py",
-        ranks="data/inter/ranks.tsv",
-        scores="data/inter/genes-standardized-scores.npy",
-        indices="data/inter/genes-standardized-scores_genes.csv",
-        A="data/output/emil_all.csv",
-        B="data/output/signatures_z10.tsv"
-    output:
-        corr="data/inter/signatures-average-correlations.npy",
-        orig="data/inter/signatures-average-correlations_origins.csv"
-    shell:
-        "python3 {input.task} {input.ranks} {input.scores} {input.indices} 10 {output.corr} {output.orig} {input.A} {input.B}"
-
-rule signatures_pvalues:
-    input:
-        task="signatures-pvalues.py",
-        ranks="data/inter/ranks.tsv",
-        corr="data/inter/signatures-average-correlations.npy",
-    output:
-        "data/inter/signatures-pvalues.npy",
-    shell:
-        "python3 {input.task} {input.ranks} {input.corr} {output}"
-
-rule clustering_corr:
-    input:
-        task="signatures-clustering.py",
-        corr="data/inter/signatures-average-correlations.npy",
-        pval="data/inter/signatures-pvalues.npy",
-        orig="data/inter/signatures-average-correlations_origins.npy"
-    output:
-        plot="data/qc/clustermap_correlations.png",
-        membership="data/inter/cluster_membership.csv"
-    shell:
-        "python3 {input.task} {input.corr} {input.orig} {input.pval} 0.01 1.5 {output.plot} {output.membership}"
-
-rule qc_jaccard:
-    input:
-        task="signatures_jaccard-distances_plot.py",
-        A="data/output/emil_all.csv",
-        B="data/output/signatures_z10.tsv"
-    output:
-        "data/qc/jaccard-distances.png"
-    shell:
-        "python3 {input.task} 10 0.5 {output} {input.A} {input.B}"
